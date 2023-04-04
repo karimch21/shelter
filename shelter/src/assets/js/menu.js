b230dbad4c7b94f554a490cfe78b51a7d4c07313
karimch21 <anonKM1999@mail.ru>
2023-04-05 01:41:29 +0300

    feat: add scripts closing menu,burger-off with click outside menu place

--- shelter/src/assets/js/menu.js
+++ shelter/src/assets/js/menu.js
@@ -2,15 +2,34 @@ const menu = document.querySelector('.menu');
 const btnMenu = document.querySelector('.burger-menu');
 
 btnMenu.addEventListener('click', () => {
-  clickBtnMenuHandler(btnMenu, menu)
+  clickBtnMenuHandler(btnMenu, menu, onOffLockScrollBody)
 });
 
-function clickBtnMenuHandler(btnMenu, menu) {
+window.addEventListener('click', clickWindowHandler)
+
+function clickBtnMenuHandler(btnMenu, menu, bodyStyle) {
   btnMenu.classList.toggle('burger-menu_active')
   menu.classList.toggle('menu_active')
-  activeLockScrollBody()
+  bodyStyle()
 }
 
-function activeLockScrollBody() {
+function onOffLockScrollBody() {
   document.body.classList.toggle('body_lock')
+}
+
+function clickWindowHandler(e) {
+  closeMenu(e, menu, btnMenu)
+}
+
+function closeMenu(e, menu, btnMenu) {
+  const targetBtnMenu = e.target.closest('.burger-menu');
+  const targetMenu = e.target.closest('.menu');
+  const targetMenuLink = e.target.closest('.menu__link');
+
+  if (targetMenuLink) {
+    clickBtnMenuHandler(btnMenu, menu, onOffLockScrollBody)
+  }
+  if (!targetMenu && !targetBtnMenu) {
+    clickBtnMenuHandler(btnMenu, menu, onOffLockScrollBody)
+  }
 }
