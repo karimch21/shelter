bb74aab1cc8f3b0fd4fcc8e1aae1f2e9321a1948
karimch21 <anonKM1999@mail.ru>
2023-04-08 01:54:35 +0300

    feat: add functional: generate unique slides when click 'next slide', add generate unique slides when page load

--- shelter/src/pages/main/slider.js
+++ shelter/src/pages/main/slider.js
@@ -3,11 +3,33 @@ import cards from '../../assets/js/bdCard'
 const btnNext = document.querySelector('.friends__slider-btn-next')
 const btnPrev = document.querySelector('.friends__slider-btn-prev')
 const sliderItemsBox = document.querySelector('.slider-box');
+let currSlides = []
+let slidesCurrName = [];
+let slidesNextName = [];
+let slidesPrevName = [];
+let nextSlides = [];
+let prevSlides = [];
+const breakpointsSlider = {
+  '1280': 3,
+  '1050': 2,
+  '600': 1
+}
+const dataSliderItem = JSON.parse(JSON.stringify(cards))
 
 btnNext.addEventListener('click', clickBtnNextHandler);
 btnPrev.addEventListener('click', clickBtnPrevHandler);
 
+window.addEventListener('load', () => {
+  generateCurrentSliderItem()
+  console.log(cards)
+})
+window.addEventListener('resize', () => {
+
+})
+
 function clickBtnNextHandler() {
+  generateNextSliderItem()
+
   const sliders = document.querySelectorAll('.slider');
   if (sliders.length > 1) sliders[0].remove()
   for (let slider of sliders) {
@@ -17,7 +39,20 @@ function clickBtnNextHandler() {
     slider.classList.add('sldier_hide-prev')
   }
 
-  appendSliderItems(createNewSliderItems(), sliderItemsBox)
+  sliderItemsBox.appendChild(createNewSliderItems())
+}
+function clickBtnPrevHandler() {
+  const sliders = document.querySelectorAll('.slider');
+  if (sliders.length > 1) sliders[sliders.length - 1].remove()
+  for (let slider of sliders) {
+    slider.classList.remove('slider_show-prev')
+    slider.classList.remove('sldier_hide-prev')
+    slider.classList.remove('.slider_show-next')
+    slider.classList.add('sldier_hide-next')
+  }
+
+  sliderItemsBox.prepend(createNewPrevSliderItems())
+
 }
 
 function createNewSliderItems() {
@@ -126,24 +161,62 @@ function createNewPrevSliderItems() {
   return c
 }
 
-function clickBtnPrevHandler() {
-  const sliders = document.querySelectorAll('.slider');
-  if (sliders.length > 1) sliders[sliders.length - 1].remove()
-  for (let slider of sliders) {
-    slider.classList.remove('slider_show-prev')
-    slider.classList.remove('sldier_hide-prev')
-    slider.classList.remove('.slider_show-next')
-    slider.classList.add('sldier_hide-next')
+function getRandomNum(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min))
+}
+
+function checkAmountSlides() {
+  const currentWidthWindow = document.documentElement.clientWidth;
+  if (currentWidthWindow > 1050) {
+    return 3
+  }
+  else if (currentWidthWindow <= 1050 && currentWidthWindow > 600) {
+    console.log(2, 'item')
+    return 2
+  }
+  console.log(1, 'item')
+  return 1
+}
+
+function generateNextSliderItem() {
+  let i = 0;
+  let amountSlide = checkAmountSlides();
+  slidesPrevName = JSON.parse(JSON.stringify(slidesCurrName))
+
+  while (i < amountSlide) {
+    let randomNum = getRandomNum(0, cards.length - 1);
+    if (!slidesCurrName.includes(cards[randomNum].name) && !slidesNextName.includes(cards[randomNum].name && !slidesPrevName.includes(cards[randomNum].name))) {
+      if (!slidesNextName.includes(cards[randomNum].name)) {
+        slidesNextName.push(cards[randomNum].name)
+        nextSlides.push(cards[randomNum])
+        i++
+      }
+    }
   }
 
-  sliderItemsBox.prepend(createNewPrevSliderItems())
-
+  if (i === amountSlide) {
+    // console.log('next', slidesNextName)
+    prevSlides = JSON.parse(JSON.stringify(currSlides))
+    currSlides = JSON.parse(JSON.stringify(nextSlides))
+    slidesPrevName = JSON.parse(JSON.stringify(slidesCurrName))
+    slidesCurrName = JSON.parse(JSON.stringify(slidesNextName))
+    // console.log('prev', slidesPrevName)
+    nextSlides = []
+    slidesNextName = []
+  }
 }
 
-function getRandomNum(max, min) {
-  return Math.round(Math.random() * (max - min))
-}
-
-function appendSliderItems(htmlElement, place) {
-  place.appendChild(htmlElement)
+function generateCurrentSliderItem() {
+  let i = 0;
+  let amountSlide = checkAmountSlides();
+  while (i < amountSlide) {
+    let randomNum = getRandomNum(0, cards.length - 1);
+    if (!slidesCurrName.includes(cards[randomNum].name)) {
+      slidesCurrName.push(cards[randomNum].name)
+      currSlides.push(cards[randomNum])
+      i++
+    }
+  }
+  console.log(slidesCurrName)
+  console.log(currSlides)
 }
