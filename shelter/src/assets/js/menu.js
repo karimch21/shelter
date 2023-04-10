caf506385cb0b693cbaceb0aa63d16f63e579653
karimch21 <anonKM1999@mail.ru>
2023-04-11 02:38:29 +0300

    feat: add function close popup when click outside popup, add: global scripts: popup, menu add in function

--- shelter/src/assets/js/menu.js
+++ shelter/src/assets/js/menu.js
@@ -1,46 +1,48 @@
-const menu = document.querySelector('.menu');
-const btnMenu = document.querySelector('.burger-menu');
+(function () {
+  const menu = document.querySelector('.menu');
+  const btnMenu = document.querySelector('.burger-menu');
 
-btnMenu.addEventListener('click', () => {
-  openMenu()
-});
+  btnMenu.addEventListener('click', () => {
+    openMenu()
+  });
 
-window.addEventListener('click', (e) => {
-  closeMenu(e)
-})
-
-
-function openMenu() {
-  btnMenu.classList.toggle('burger-menu_active')
-  menu.classList.toggle('menu_active')
-  document.body.classList.toggle('body_lock')
-}
+  window.addEventListener('click', (e) => {
+    closeMenu(e)
+  })
 
 
-function onLockScrollBody() {
-  document.body.classList.add('body_lock')
-}
-function offLockScrollBody() {
-  document.body.classList.remove('body_lock')
-}
-
-
-function closeMenu(e) {
-  const targetBtnMenu = e.target.closest('.burger-menu');
-  const targetMenu = e.target.closest('.menu');
-  const targetMenuLink = e.target.closest('.menu__link');
-
-  if (targetMenuLink) {
-    removeOpenStylesMenu()
+  function openMenu() {
+    btnMenu.classList.toggle('burger-menu_active')
+    menu.classList.toggle('menu_active')
+    document.body.classList.toggle('body_lock')
   }
-  if (!targetMenu && !targetBtnMenu) {
-    removeOpenStylesMenu()
+
+
+  function onLockScrollBody() {
+    document.body.classList.add('body_lock')
   }
-}
+  function offLockScrollBody() {
+    document.body.classList.remove('body_lock')
+  }
 
 
-function removeOpenStylesMenu() {
-  btnMenu.classList.remove('burger-menu_active')
-  menu.classList.remove('menu_active')
-  offLockScrollBody()
-}
+  function closeMenu(e) {
+    const targetBtnMenu = e.target.closest('.burger-menu');
+    const targetMenu = e.target.closest('.menu');
+    const targetMenuLink = e.target.closest('.menu__link');
+
+    if (targetMenuLink) {
+      removeOpenStylesMenu()
+    }
+    if (!targetMenu && !targetBtnMenu) {
+      removeOpenStylesMenu()
+    }
+  }
+
+
+  function removeOpenStylesMenu() {
+    btnMenu.classList.remove('burger-menu_active')
+    menu.classList.remove('menu_active')
+    offLockScrollBody()
+  }
+})()
